f47b006b4e8faf35246b82fa573f5f2c248d1e41
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2015-09-29 21:55:21 +0200

    Se mueven las clases al paquete principal

---
+++ src/main/java/sergio/gil/ecp1/miw/upm/es/C11.java
@@ -0,0 +1,13 @@
+package sergio.gil.ecp1.miw.upm.es;
+
+public class C11 {
+	
+	public String m1(){
+		return ("m1");
+	}
+	
+	public String m2(){
+		return ("m2");
+	}
+
+}
---
+++ src/main/java/sergio/gil/ecp1/miw/upm/es/C12.java
@@ -0,0 +1,9 @@
+package sergio.gil.ecp1.miw.upm.es;
+
+public class C12 {
+
+	public String mA() {
+		return ("mA");
+	}
+
+}
