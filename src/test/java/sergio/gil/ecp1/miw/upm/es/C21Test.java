e5c0e46fdc7730c3e50da21ba7b1989e2f5ce571
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2015-09-29 20:41:10 +0200

    Creacion de la clase test comprobando la clase anteriormente subida C21

---
+++ src/test/java/sergio/gil/ecp1/miw/upm/es/C21Test.java
@@ -0,0 +1,28 @@
+package sergio.gil.ecp1.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class C21Test {
+	
+	public C21 c;
+	
+	@Before
+    public void before() {
+        c = new C21();
+    }
+
+	@Test
+	public void testM1() {
+		assertEquals("m1", c.m1());
+	}
+
+	@Test
+	public void testM2() {
+		assertEquals("m2", c.m2());
+	}
+
+}
